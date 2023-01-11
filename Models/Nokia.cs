5567fea0265e2ee705f010bc4695e5a02ae8f9c5
antoniomarcelino <antoniomarcelinodev@gmail.com>
2023-01-11 17:35:28 -0300

    desafio de projeto

---
+++ Models/Nokia.cs
@@ -0,0 +1,20 @@
+namespace DesafioPOO.Models
+{
+    
+    public class Nokia : Smartphone
+    {
+
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine("Instalando " + nomeApp);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Testando NOKIA");
+Nokia nokia  = new Nokia("981982023", "NOKIA2023", "1111", 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram foi instalado.");
+
+Console.WriteLine("");
+
+Console.WriteLine("Testando IPHONE");
+Iphone iphone = new Iphone("998812023", "IPHONE2023", "1234", 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
+Console.WriteLine("");
